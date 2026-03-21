596d62c631f3e460499d362a55199a68a4a010b7
Tadgh <tadgh@tadghwagstaff.com>
2026-03-21 17:00:36 +0000

    add logging to deploy/copy scripts and make sure both check for existing catppuccin-tmux install

--- copy-config.sh
+++ copy-config.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+echo "Copying config to client..."
 mkdir -p ~/.config ~/.config/alacritty ~/.config/nvim ~/.config/rio ~/.config/tmux
 cp -r alacritty nvim rio tmux ~/.config/
 cp .bash_profile .bashrc .gitconfig ~/
-git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+echo "Config copied."
+if [[ -d ~/dotfiles/tmux/plugins/catppuccin/tmux ]]; then
+  echo "Catppuccin theme for tmux installed, skipping clone."
+else
+  echo "Catppuccin theme for tmux not found, fetching..."
+  git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+fi
+echo "Done!"
+
--- deploy-config.sh
+++ deploy-config.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "Config deployed."
+if [[ -d ~/dotfiles/tmux/plugins/catppuccin/tmux ]]; then
+  echo "Catppuccin theme for tmux installed, skipping clone."
+else
+  echo "Catppuccin theme for tmux not found, fetching..."
+  git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+fi
+echo "Done!"
 
-chmod +x ./stash-config.sh ./symlink-config.sh
-./stash-config.sh
-./symlink-config.sh
-git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
