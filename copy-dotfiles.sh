26164599510a59ee95b2eea30bda88f5d2466184
Tadgh <tadgh@tadghwagstaff.com>
2026-03-21 12:00:00 +0000

    fixes to deploy scripts (more to go)

--- copy-dotfiles.sh
+++ copy-dotfiles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p ~/.config ~/.config/alacritty ~/.config/nvim ~/.config/rio ~/.config/tmux
 cp -r alacritty nvim rio tmux ~/.config/
 cp .bash_profile .bashrc .gitconfig ~/
--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 mkdir -p ~/.config
-ln -s $(realpath -- ".bash_profile") ~/
-ln -s $(realpath -- ".bashrc") ~/
-ln -s $(realpath -- "tmux") ~/.config/tmux
-ln -s $(realpath -- "nvim") ~/.config/nvim
-ln -s $(realpath -- "rio") ~/.config/rio
-ln -s $(realpath -- "alacritty") ~/.config/alacritty
+ln -s $(realpath ".bash_profile") ~/
+ln -s $(realpath ".bashrc") ~/
+ln -s $(realpath ".gitconfig") ~/
+ln -s $(realpath "tmux") ~/.config/tmux
+ln -s $(realpath "nvim") ~/.config/nvim
+ln -s $(realpath "rio") ~/.config/rio
+ln -s $(realpath "alacritty") ~/.config/alacritty
